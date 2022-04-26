13e4385cc32969a98d74edc016f2db7af3d7bad9
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-26 13:58:10 -0500

    chore(ui): Added PrimeVue UI Library Configuration.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
 
-const app = createApp(App)
+// PrimeVue Theme
+import "primevue/resources/themes/md-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+// PrimeVue configuration
+app.use(PrimeVue, { ripple: true});
+
+app.mount("#app");
