c4348b2e0ad4bae9d03958065a6d742f59d95cf0
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-28 13:46:52 -0500

    chore: Added complementary UI Components Set, and established in-app navigation routes.

--- src/main.js
+++ src/main.js
@@ -13,13 +13,22 @@ import Button from "primevue/button";
 import Toolbar from "primevue/toolbar";
 import Dialog from "primevue/dialog";
 import InputText from "primevue/inputtext";
+import ToastService from "primevue/toastservice";
+import Toast from "primevue/toast";
+import Sidebar from "primevue/sidebar";
+import Dropdown from "primevue/dropdown";
+import Tag from "primevue/tag";
 
 const app = createApp(App);
 
+// Routing Management
 app.use(router);
 
 // PrimeVue configuration
 app.use(PrimeVue, { ripple: true });
+
+// Toast Service
+app.use(ToastService);
 
 // PrimeVue Components
 app.component("pv-data-table", DataTable);
@@ -28,5 +37,9 @@ app.component("pv-button", Button);
 app.component("pv-toolbar", Toolbar);
 app.component("pv-dialog", Dialog);
 app.component("pv-input-text", InputText);
+app.component("pv-toast", Toast);
+app.component("pv-sidebar", Sidebar);
+app.component("pv-dropdown", Dropdown);
+app.component("pv-tag", Tag);
 
 app.mount("#app");
